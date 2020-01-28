1e5bb268f6f7aa5c76d26af245d08e8a4ad7f105
Nathan Bonnemains <bonnemainsnathan@gmail.com>
2020-01-28 18:58:08 +0100

    Add Travis

--- td1/src/main/java/s4/spring/td1/controllers/HelloController.java
+++ td1/src/main/java/s4/spring/td1/controllers/HelloController.java
@@ -3,7 +3,6 @@ package s4.spring.td1.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
