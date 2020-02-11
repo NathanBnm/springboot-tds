a6e995ccdf32fc61f01b0a3c0e666208d5c3a542
Nathan Bonnemains <bonnemainsnathan@gmail.com>
2020-02-11 15:12:27 +0100

    Remplacement des SesssionAttributes par l'utilisation de la base de données H2 et fonction de recherche

--- src/main/java/s4/spring/td2/controllers/OrgaController.java
+++ src/main/java/s4/spring/td2/controllers/OrgaController.java
@@ -1,26 +1,24 @@
 package s4.spring.td2.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 import s4.spring.td2.entities.Organization;
+import s4.spring.td2.repositories.OrgaRepository;
 
-import java.sql.Ref;
-import java.util.ArrayList;
 import java.util.List;
 
-@Controller
-@SessionAttributes("orgas")
-public class OrgaController {
 
-    @ModelAttribute("orgas")
-    public List<Organization> getItems(){
-        return new ArrayList<>();
-    }
-    
+@Controller
+public class OrgaController {
+    @Autowired
+    private OrgaRepository repo;
+     
     @GetMapping("/orgas/index")
-    public String viewIndex() {
+    public String viewIndex(ModelMap model) {
+        model.put("orgas", repo.findAll());
         return "index";
     }
 
@@ -30,8 +28,8 @@ public class OrgaController {
     }
 
     @GetMapping("/orgas/edit/{id}")
-    public String viewEdit(@PathVariable int id, @SessionAttribute List<Organization> orgas, ModelMap model) {
-        Organization orga = orgas.get(id);
+    public String viewEdit(@PathVariable("id") String id, ModelMap model) {
+        Organization orga = repo.findById(Integer.parseInt(id));
         model.put("id", orga.getId());
         model.put("name", orga.getName());
         model.put("domain", orga.getDomain());
@@ -40,16 +38,17 @@ public class OrgaController {
     }
 
     @GetMapping("/orgas/delete/{id}")
-    public String viewDelete(@PathVariable int id, @SessionAttribute List<Organization> orgas, ModelMap model) {
-        Organization orga = orgas.get(id);
-        model.put("id", orga.getId());
+    public String viewDelete(@PathVariable("id") String id, ModelMap model) {
+        Organization orga = repo.findById(Integer.parseInt(id));
+        model.put("orgas", repo.findAll());
+        model.put("delete", true);
         model.put("domain", orga.getDomain());
         return "index";
     }
-
+    
     @GetMapping("/orgas/display/{id}")
-    public String viewDisplay(@PathVariable int id, @SessionAttribute List<Organization> orgas, ModelMap model) {
-        Organization orga = orgas.get(id);
+    public String viewDisplay(@PathVariable("id") String id, ModelMap model) {
+        Organization orga = repo.findById(Integer.parseInt(id));
         model.put("id", orga.getId());
         model.put("name", orga.getName());
         model.put("domain", orga.getDomain());
@@ -58,23 +57,31 @@ public class OrgaController {
     }
 
     @PostMapping("/orgas/add-new")
-    public RedirectView addNew(@RequestParam String name, @RequestParam String domain, @RequestParam String aliases, @SessionAttribute List<Organization> orgas) {
-        orgas.add(new Organization(name, domain, aliases));
+    public RedirectView addNew(@RequestParam String name, @RequestParam String domain, @RequestParam String aliases) {
+        repo.save(new Organization(name, domain, aliases));
         return new RedirectView("/orgas/index");
     }
     
     @PostMapping("/orgas/edit")
-    public RedirectView edit(@RequestParam int id, @RequestParam String name, @RequestParam String domain, @RequestParam String aliases, @SessionAttribute List<Organization> orgas) {
-        Organization orga = orgas.get(id);
+    public RedirectView edit(@RequestParam("id") String id, @RequestParam String name, @RequestParam String domain, @RequestParam String aliases) {
+        Organization orga = repo.findById(Integer.parseInt(id));
         orga.setName(name);
         orga.setDomain(domain);
         orga.setAliases(aliases);
+        repo.save(orga);
         return new RedirectView("/orgas/index");
     }
     
     @PostMapping("/orgas/delete")
-    public RedirectView delete(@RequestParam int id, @SessionAttribute List<Organization> orgas) {
-        orgas.remove(id);
+    public RedirectView delete(@RequestParam("id") String id) {
+        Organization orga = repo.findById(Integer.parseInt(id));
+        repo.delete(orga);
         return new RedirectView("/orgas/index");
+    }
+    
+    @PostMapping("/orgas/search")
+    public String search(@RequestParam String search, ModelMap model) {
+        model.put("orgas", repo.findBySearch(search));
+        return "index";
     }
 }
