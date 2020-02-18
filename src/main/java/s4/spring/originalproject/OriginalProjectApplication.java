884f2195d813f5e20e1b8f80d4d36dbd9ac72006
Nathan Bonnemains <bonnemainsnathan@gmail.com>
2020-02-18 14:24:23 +0100

    Add original template

---
+++ src/main/java/s4/spring/originalproject/OriginalProjectApplication.java
@@ -0,0 +1,13 @@
+package s4.spring.originalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OriginalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OriginalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/s4/spring/originalproject/OriginalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package s4.spring.originalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OriginalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
