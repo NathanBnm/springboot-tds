81c702b875ed0902f1214e509b96e88dda8d82fb
Nathan Bonnemains <bonnemainsnathan@gmail.com>
2020-03-23 15:49:29 +0100

    First commit

---
+++ src/test/java/iutc3/unicaen/fr/td5/Td5ApplicationTests.java
@@ -0,0 +1,13 @@
+package iutc3.unicaen.fr.td5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Td5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
