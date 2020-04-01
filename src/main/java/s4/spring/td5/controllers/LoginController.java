98070f91abaf43962a7a75a436109941bdd52a19
Nathan Bonnemains <bonnemainsnathan@gmail.com>
2020-04-01 11:59:31 +0200

    Connexion fonctionnelle + Message de bienvenue

--- src/main/java/s4/spring/td5/controllers/LoginController.java
+++ src/main/java/s4/spring/td5/controllers/LoginController.java
@@ -3,7 +3,6 @@ package s4.spring.td5.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.RedirectView;
@@ -21,20 +20,18 @@ public class LoginController {
     public String login() {
         if (userRepository.findAll().size() == 0) {
             User user = new User("NathanBnm", "azerty", "bonnemainsnathan@gmail.com", "Nathan Bonnemains");
-            userRepository.save(user);
+            userRepository.saveAndFlush(user);
         }
         return "login";
     }
 
     @PostMapping("/doLogin")
-    public RedirectView doLogin(@RequestParam String login, @RequestParam String password, HttpSession session, @ModelAttribute User connectedUser) {
+    public RedirectView doLogin(@RequestParam String login, @RequestParam String password, HttpSession session) {
         if (!"".equals(login) && !"".equals(password)) {
-            System.out.println("|" + login + "|");
             User user = userRepository.findByLogin(login);
-            System.out.println(user);
             if (user != null) {
                 if (user.getPassword().equals(password)) {
-                    session.setAttribute("connectedUser", connectedUser);
+                    session.setAttribute("connectedUser", user);
                     return new RedirectView("/index");
                 } else {
                     return new RedirectView("/login");
--- src/main/java/s4/spring/td5/controllers/MainController.java
+++ src/main/java/s4/spring/td5/controllers/MainController.java
@@ -1,12 +1,25 @@
 package s4.spring.td5.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import s4.spring.td5.models.User;
+import s4.spring.td5.repositories.UserRepository;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class MainController {
+    @Autowired
+    UserRepository userRepository;
+
     @RequestMapping(value = {"", "/", "index"})
-    public String index() {
+    public String index(ModelMap model, HttpSession session) {
+        User connectedUser = (User) session.getAttribute("connectedUser");
+        System.out.println(connectedUser);
+        if (connectedUser != null)
+            model.put("message", "Vous êtes connecté en tant que " + connectedUser.getIdentity() + ".");
         return "index";
     }
 }
