4b2b09e4ed2587d83a797e967fdc24136c722597
Nathan Bonnemains <bonnemainsnathan@gmail.com>
2020-04-02 18:11:37 +0200

    Ajout tableau des scripts avec recherche

---
+++ src/main/java/s4/spring/td6/controllers/VueScriptController.java
@@ -0,0 +1,35 @@
+package s4.spring.td6.controllers;
+
+import io.github.jeemv.springboot.vuejs.VueJS;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import s4.spring.td6.models.Script;
+import s4.spring.td6.repositories.ScriptRepository;
+
+import java.util.List;
+
+@Controller
+public class VueScriptController {
+    @Autowired
+    private ScriptRepository scriptRepository;
+
+    @Autowired
+    private VueJS vue;
+
+    @GetMapping("/search")
+    public String searchView(ModelMap model) {
+        String rest = "/rest/scripts";
+        List<Script> scripts = scriptRepository.findAll();
+        vue.addDataRaw("headers", "[" +
+                "{text: 'Nom', value: 'title'}," +
+                "{text: 'Catégorie', value: 'category'}," +
+                "{text: 'Langage', value: 'language'}" +
+                "]");
+        vue.addData("scripts", scripts);
+        vue.addData("search", "");
+        model.put("vue", this.vue);
+        return "search";
+    }
+}
