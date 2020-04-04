3b1afbb8fb0f93db314f11cbcf5b37d4d192c51b
Nathan Bonnemains <bonnemainsnathan@gmail.com>
2020-04-04 18:37:04 +0200

    Sélection des colonnes à afficher/rechercher fonctionelle

--- src/main/java/s4/spring/td6/controllers/VueScriptController.java
+++ src/main/java/s4/spring/td6/controllers/VueScriptController.java
@@ -5,10 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import s4.spring.td6.models.Script;
 import s4.spring.td6.repositories.ScriptRepository;
-
-import java.util.List;
 
 @Controller
 public class VueScriptController {
@@ -21,36 +18,21 @@ public class VueScriptController {
     @GetMapping("/search")
     public String searchView(ModelMap model) {
         String rest = "/rest/scripts";
-        List<Script> scripts = scriptRepository.findAll();
-
-        vue.addData("hideTitle", false);
-        vue.addData("hideDescription", false);
-        vue.addData("hideContent", false);
-        vue.addData("hideCategory", false);
-        vue.addData("hideLanguage", false);
-
-        vue.addData("search", "");
-        vue.addDataRaw("checkedColumns", "['name', 'description', 'content', 'category', 'language']");
 
         vue.addDataRaw("headers", "[" +
                 "{text: 'Nom', value: 'title'}," +
                 "{text: 'Description', value: 'description'}," +
                 "{text: 'Contenu', value: 'content'}," +
-                "{text: 'Catégorie', value: 'category'}," +
-                "{text: 'Langage', value: 'language'}" +
+                "{text: 'Catégorie', value: 'category.name'}," +
+                "{text: 'Langage', value: 'language.name'}" +
                 "]");
+        vue.addDataRaw("selectedHeaders", "['title', 'description', 'content', 'category.name', 'language.name']");
 
-        vue.addData("scripts", scripts);
+        vue.addData("search", "");
+        vue.addData("scripts", scriptRepository.findAll());
 
-        vue.addComputed("computedHeaders", "return this.hideTitle ? this.headers.filter(header => header.value !== 'title') : this.headers;");
-
+        vue.addComputed("computedHeaders", "return this.headers.filter((header => this.selectedHeaders.includes(header.value)));");
         vue.addComputed("colspan", "return this.headers.length;");
-
-        vue.addMethod("hideTitleToggle", "this.hideTitle = !this.hideTitle");
-        vue.addMethod("hideDescriptionToggle", "this.hideDescription = !this.hideDescription");
-        vue.addMethod("hideContentToggle", "this.hideContent = !this.hideContent");
-        vue.addMethod("hideCategoryToggle", "this.hideCategory = !this.hideCategory");
-        vue.addMethod("hideLanguageToggle", "this.hideLanguage = !this.hideLanguage");
 
         model.put("vue", this.vue);
         return "search";
